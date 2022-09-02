48e54375c55f2a6672bb7f6a5533e68a7dc1668a
danieldevcode <danieldevcode@gmail.com>
2022-09-01 19:56:00 -0500

    Bug fixes
    - Fixed main screen content

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+import ProductosView from './vistas/productos.js';
+import ProductosAdd from './vistas/productos_add';
+
+function MyStack(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <Stack.Navigator>
+    <Stack.Screen name="productos" component={ProductosView} options={{
+        title: "Listado de Productos",
+        headerTitleAlign: "center",
+    }}/>
+    <Stack.Screen name="productos_add" component={ProductosAdd}/>
+   </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+    return(
+        <NavigationContainer>
+            <MyStack />
+        </NavigationContainer>
+    );
+}
+
+export default App;
